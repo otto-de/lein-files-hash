4323befacec344b103e45359beb5d96d861050ac
Svante von Erichsen <svante.vonerichsen@otto.de>
2019-06-21 13:12:35 +0200

    [[version][sve]] v 0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-files-hash "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject lein-files-hash "0.1.0"
+  :description "A Leiningen plugin to save hash trees of files and directories to other files."
+  :url "https://github.com/otto-ec/nav_lein-files-hash"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :eval-in-leiningen true
