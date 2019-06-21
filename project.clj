6625aa928254704ebd6d43e4acceb4ae1302b905
Svante von Erichsen <svante.vonerichsen@otto.de>
2019-06-21 14:17:12 +0200

    [[version][sve]] snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-files-hash "0.1.1"
+(defproject lein-files-hash "0.1.2-SNAPSHOT"
   :description "A Leiningen plugin to save hash trees of files and directories to other files."
   :url "https://github.com/otto-ec/nav_lein-files-hash"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
