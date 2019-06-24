162de9026b30e818a0b46da44d78ab08dda9b723
Svante von Erichsen <svante.vonerichsen@otto.de>
2019-06-24 17:22:17 +0200

    [[license][sve]] Apache license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject lein-files-hash "0.1.2-SNAPSHOT"
   :description "A Leiningen plugin to save hash trees of files and directories to other files."
   :url "https://github.com/otto-ec/nav_lein-files-hash"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "Apache License 2.0"
+            :url  "http://www.apache.org/license/LICENSE-2.0.html"}
   :eval-in-leiningen true
 
   :dependencies [[org.clojure/clojure "1.10.0"]]
